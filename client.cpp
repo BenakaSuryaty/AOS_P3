8c7d68f461d709b81aa7e2423b537ab9ba31bf8b
fjeroan <81400801+fjeroan@users.noreply.github.com>
2024-05-06 22:13:10 -0500

    Updated the client code
    Let me know if this works, I'll clean the code later!

--- client.cpp
+++ client.cpp
@@ -31,6 +31,10 @@
 #include <ctime>
 #include <fstream>
 #include <iomanip>
+<<<<<<< Updated upstream
+=======
+#include <sys/time.h>
+>>>>>>> Stashed changes
 
 using namespace std;
 using namespace std::chrono;
@@ -139,7 +143,7 @@ class ThreadPool
 
 };
 
-
+//functions used
 int hashFunction(const std::string& obj);
 
 string createFile();
@@ -151,22 +155,18 @@ int clientRead(int hashNum, string fileName);
 void converStringToChar(char* timeChar, string timeStr);
 
 string getkeyVal(const std::string& obj);
+<<<<<<< Updated upstream
+=======
+
+int clientWrite(int hashVal, string fileName);
+>>>>>>> Stashed changes
 
 int main(){
 
 	//variable for menu selection
 	int selection;
-	cout << "Hello" << endl;
 
 	
-	//creating the threadpool
-	ThreadPool pool{10};
-	
-	pool.enqueue([] {
-		cout << "Hello from thread" << endl;		
-	});
-
-	cout << hashFunction("Hello");
 	
 	while(true){
 
@@ -182,7 +182,35 @@ int main(){
 		//code for menu
 		if(selection == 1)
 		{
-			cout << hashFunction(createFile()) << endl;
+				
+			//getting fileName and value for hash
+			int hashNum;
+			string fileName;
+			
+			cout<< "Enter file you'd like to read: ";
+			cin >> fileName;
+			readFile(fileName);
+
+			//getting the first hashed value
+			hashNum = hashFunction(fileName);
+
+			//attempting to connect with the clients
+			if(clientWrite(hashNum %7, fileName) < 0)
+			{
+				cout << "first server down unreachable, connecting to next" << endl;
+				if(clientWrite((hashNum +2) % 7, fileName) < 0)
+				{
+					cout << "second server unreachable, connecting to next" << endl;
+					
+					if(clientWrite((hashNum +4) % 7, fileName) < 0)
+					{
+						cout << "None of the servers are reachable, aborting write" << endl;
+					}
+				}
+			}
+			
+
+
 
 		}
 		else if (selection ==2)
@@ -357,9 +385,47 @@ int clientRead(int hashVal, string fileName)
      }
 	
 
+<<<<<<< Updated upstream
      //waiting for ack
       n = read(sockfd,buffer,255);
+=======
+     //handshake protocol
+     //
+    fd_set socketfd;
+    FD_ZERO(&socketfd);
+    FD_SET(sockfd, &socketfd);
 
+    //setting timeoutvalue
+    struct timeval timeout;
+    timeout.tv_sec = 5;
+    timeout.tv_usec = 0;
+	
+      n = write(sockfd, "Sending ack", 20);
+>>>>>>> Stashed changes
+
+
+      //waiting for ack message
+      int ret = select(sockfd+1, &socketfd, NULL, NULL, &timeout);
+
+      //creating the request
+      char request_char[100];
+
+<<<<<<< Updated upstream
+      //getting the timestamp
+      milliseconds ms = duration_cast< milliseconds >(
+		    system_clock::now().time_since_epoch()
+		);
+
+=======
+      if(ret > 0){
+
+     	 n = read(sockfd,buffer,255);
+      }
+      else
+      {
+	cout << "No message from server, abort" << endl;
+	return -1;
+      }
 
 
       //creating the request
@@ -370,6 +436,7 @@ int clientRead(int hashVal, string fileName)
 		    system_clock::now().time_since_epoch()
 		);
 
+>>>>>>> Stashed changes
       string timestampString  = to_string(ms.count());
 
       //getting unique keyvalue from hashing fileName
@@ -385,6 +452,7 @@ int clientRead(int hashVal, string fileName)
         
 
 }
+<<<<<<< Updated upstream
 
 void converStringToChar(char* timeChar, string timeStr){
 	    
@@ -395,6 +463,127 @@ void converStringToChar(char* timeChar, string timeStr){
 
 
 
+=======
+
+
+//function to send write request
+int clientWrite(int hashVal, string fileName)
+{
+
+
+	//arr storying all server names and portnumbers
+      int portArr[7] = {2312, 2313, 2314, 2315, 2316, 2317,2318};
+	string machineName[7] = {"dc01.utdallas.edu", "dc02.utdallas.edu", "dc03.utdallas.edu", "dc04.utdallas.edu", "dc05.utdallas.edu", "dc06.utdallas.edu", "dc07.utdallas.edu"};
+
+
+//	storing the variables needed to create client
+     int sockfd, portno, n;
+     
+     struct sockaddr_in serv_addr;
+     
+     struct hostent *server;
+
+
+
+     char buffer[256];
+
+    //creating client
+     portno = (portArr[hashVal]);
+
+     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+     if (sockfd < 0) {
+
+	    printf("ERROR opening socket");
+	    return -1;
+     }
+
+     server = gethostbyname(machineName[hashVal].c_str());
+
+     if (server == NULL) {
+
+	     fprintf(stderr,"ERROR, no such host\n");
+	     return -1;
+
+     }
+
+     bzero((char *) &serv_addr, sizeof(serv_addr));
+
+     serv_addr.sin_family = AF_INET;
+
+     bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+
+     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+     {
+	     printf("ERROR connecting");
+	     return -1;
+     }
+	
+
+     //handshake protocol
+     //
+    fd_set socketfd;
+    FD_ZERO(&socketfd);
+    FD_SET(sockfd, &socketfd);
+
+    //setting timeoutvalue
+    struct timeval timeout;
+    timeout.tv_sec = 5;
+    timeout.tv_usec = 0;
+	
+      n = write(sockfd, "Sending ack", 20);
+
+
+      //waiting for ack message
+      int ret = select(sockfd+1, &socketfd, NULL, NULL, &timeout);
+
+
+      if(ret > 0){
+
+     	 n = read(sockfd,buffer,255);
+      }
+      else
+      {
+	cout << "No message from server, abort" << endl;
+	return -1;
+      }
+
+
+      //creating the request
+      char request_char[100];
+
+      //getting the timestamp
+      milliseconds ms = duration_cast< milliseconds >(
+		    system_clock::now().time_since_epoch()
+		);
+
+      string timestampString  = to_string(ms.count());
+
+      //getting unique keyvalue from hashing fileName
+     string key = getkeyVal(fileName);
+
+    string request = timestampString + ":write:" + key;
+
+	converStringToChar(request_char, request); 
+
+  	n = write(sockfd, request_char,100);
+
+
+        
+
+}
+
+
+void converStringToChar(char* timeChar, string timeStr){
+	    
+	for(int i=0; i < timeStr.length(); ++i){
+		        timeChar[i] = timeStr[i];
+			  }
+}
+
+
+
+>>>>>>> Stashed changes
 string getkeyVal(const std::string& obj) {
 	    // Calculate the hash value based on the object
      int sum = 0;
