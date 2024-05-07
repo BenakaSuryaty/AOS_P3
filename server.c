2a15653619f79f07ae24e62bbdf9bfcfabcc99c3
BenakaSuryaty <benaka.suryaty@gmail.com>
2024-05-06 23:31:52 -0500

    (working) handshake functionality example

--- server.c
+++ server.c
@@ -25,7 +25,8 @@
 #define SERVER_PORT 4000 //[ ]: change to dc machine's available port
 #define SERVER_IP "127.0.0.1"//[ ]: change to dc machine's IP
 #define SERVER_BACKLOG 6
-#define THREAD_POOL_SIZE 10
+#define THREAD_POOL_SIZE 15
+#define MAX_HANDSHAKE_MSG_SIZE 1024
 
 //struct def to store address info
 typedef struct sockaddr_in SA_IN;
@@ -35,12 +36,74 @@ typedef struct sockaddr SA;
 pthread_mutex_t eventQueue_mutex;
 pthread_cond_t eventQueue_cond;
 
-//event queue
-
-
 
 //NOTE: helper funcitons
 
+
+//event loop function to check for pending client in the queue
+//[ ] complete this function
+void * EventLoop(void * arg){
+    while(true){
+        int *pclient = dequeue();
+        if(pclient != NULL){
+            //some function to handle connection
+            continue;
+            /*handle client function*/
+        }
+    }
+}
+
+//[ ] edit this to match the ack/non-ack message structure
+bool handshake_protocol(int client_sock, char* addr) {
+    char server_msg[MAX_HANDSHAKE_MSG_SIZE];
+    char client_msg[MAX_HANDSHAKE_MSG_SIZE];
+    ssize_t bytes_received, bytes_sent;
+
+    //handshake init
+    printf("Handshake initiated.\n");
+
+    // Construct the server hello message with the client's IP address
+    char server_hello[MAX_HANDSHAKE_MSG_SIZE];
+    snprintf(server_hello, MAX_HANDSHAKE_MSG_SIZE, "Hello client with IP: %s! Please send your handshake message.\n", addr);
+
+    // Step 1: Server sends the handshake message
+    bytes_sent = send(client_sock, server_hello, strlen(server_hello), 0);
+    if (bytes_sent == -1) {
+        perror("send");
+        return false;
+    }
+
+    // Step 2: Client receives the handshake message
+    bytes_received = recv(client_sock, client_msg, sizeof(client_msg), 0);
+    if (bytes_received == -1) {
+        perror("recv");
+        return false;
+    }
+    client_msg[bytes_received] = '\0';
+    printf("Client: %s\n", client_msg);
+
+    // Step 3: Client sends its handshake response
+    const char* client_response = "Received server hello. Handshake complete.\n";
+    bytes_sent = send(client_sock, client_response, strlen(client_response), 0);
+    if (bytes_sent == -1) {
+        perror("send");
+        return false;
+    }
+
+    // Step 4: Server receives client's handshake response
+    bytes_received = recv(client_sock, server_msg, sizeof(server_msg), 0);
+    if (bytes_received == -1) {
+        perror("recv");
+        return false;
+    }
+    server_msg[bytes_received] = '\0';
+    printf("Client: %s\n", server_msg);
+
+    printf("Handshake complete.\n");
+
+    // Handshake complete
+    return true;
+}
 //func to check the errors
 int check(int exp, const char *msg){
     if(exp == ERROR){
@@ -52,7 +115,7 @@ int check(int exp, const char *msg){
 
 
 
-//TODO: Add helper functions for the main logic
+//[ ] Add helper functions for the main logic
 
 int main(){
     //hash to store key value pair
@@ -63,17 +126,18 @@ int main(){
     pthread_t th[THREAD_POOL_SIZE];
     pthread_mutex_init(&eventQueue_mutex, NULL);
     pthread_cond_init(&eventQueue_cond, NULL);
-  /*
+  
     for (int i = 0; i < THREAD_POOL_SIZE; i++) {
-        if (pthread_create(&th[i], NULL, , NULL) != 0) { //[ ]: fix according to the helper functions.
+        if (pthread_create(&th[i], NULL,EventLoop, NULL) != 0) { //[x]: fix according to the helper functions.
             perror("Failed to create the thread");
         }
     }
-*/
+
 
     //socket creation
-    int server_sock, client_sock, addr_size;
+    int server_sock, client_sock,addr_size;
     SA_IN server_addr, client_addr;
+    char c_addr[INET_ADDRSTRLEN];//to display the connect client's address
 
     check((server_sock = socket(AF_INET, SOCK_STREAM, 0)),"Failed to create socket.");
 
@@ -88,7 +152,30 @@ int main(){
 
     while (true)
     {
-        //TODO: main logic
+        printf("Waiting for connections...\n");
+        //waiting and accepting connections from the clients
+        addr_size = sizeof(SA_IN);
+        check(client_sock =
+                accept(server_sock, (SA*)&client_addr,&addr_size),"Accept failed!");
+        
+        inet_ntop(AF_INET, &client_addr.sin_addr,c_addr,INET_ADDRSTRLEN);
+        //displaying connected client info
+        printf("Client connected!(IP address: %s)\n",c_addr);
+
+        int *pclient = malloc(sizeof(int));
+        *pclient = client_sock;
+        //[ ]handling the connected client
+        /*
+        bool agree_disagree=handshake_protocol(client_sock,c_addr);
+
+        if (agree_disagree)
+        {
+            continue;
+        }*/
+
+
+        
+
     }
     
     //joining threads and destroying pthread vars
