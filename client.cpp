00d0a66b2a0308f450375db530dfd8829407f6a0
fjeroan <81400801+fjeroan@users.noreply.github.com>
2024-05-07 02:27:38 -0500

    Client new
    Cleaned up the code, implemented the handshake the new key functions

--- client.cpp
+++ client.cpp
@@ -37,108 +37,9 @@ using namespace std;
 using namespace std::chrono;
 using namespace std::this_thread;
 
-//creating another threadpool to handle all the different threads
-class ThreadPool
-{
-	public:
-		using Task = function<void()>;
-
-		template<class T>
-		auto enqueue(T task)->future<decltype(task())>
-		{
-
-			auto wrapper = make_shared<packaged_task<decltype(task()) ()> >(move(task));
-		
-			{
-				unique_lock<mutex> lock{EventMutex};
-				tasks.emplace([=] {
-						(*wrapper)();
-						});
-			}
-			
-			mEventVar.notify_one();
-
-			return wrapper->get_future();
-
-		
-		
-		}
 
 
-		explicit ThreadPool(size_t numThreads)
-		{
-			start(numThreads);
-
-		}
-
-		~ThreadPool()
-		{
-
-			stop();
-
-		}
-
-
-
-	private:
-		queue<Task> tasks;
-
-		condition_variable mEventVar;
-
-		mutex EventMutex;
-
-		bool stopping = false;	
-		vector <thread> mthreads;
-	void start(size_t numThreads)
-	{
-		for(auto i =0; i < numThreads; i++)
-		{	
-			mthreads.emplace_back([=] {
-
-				while(true)
-				{
-					Task task;
-
-					{
-						unique_lock<mutex> lock{EventMutex};
-
-						mEventVar.wait(lock, [=] {return stopping || !tasks.empty();});
-
-						if(stopping && tasks.empty() )
-							break;
-
-						task =move(tasks.front());
-						tasks.pop();
-							
-					}
-
-					task();		
-				}
-				
-					
-					
-			});
-
-
-		}
-
-	}
-
-	void stop() noexcept
-	{
-		{
-			unique_lock<mutex> lock{EventMutex};
-			stopping = true;
-		}
-		mEventVar.notify_all();
-		
-		for(auto &thread:mthreads)
-		{
-			thread.join();
-		}
-	}
-
-};
+#define MAX_HANDSHAKE_MSG_SIZE 1024
 
 //functions used
 int hashFunction(const std::string& obj);
@@ -155,6 +56,10 @@ string getkeyVal(const std::string& obj);
 
 int clientWrite(int hashVal, string fileName);
 
+int clientInsert(int hashVal, string keyName);
+
+bool handshake_protocol(int server_sock); 
+
 int main(){
 
 	//variable for menu selection
@@ -166,89 +71,130 @@ int main(){
 
 
 		//outputting menu
-		cout << "\n1. Create and edit a file" << endl;
-		cout << "2. Read a file" << endl;
-		cout << "3. Edit a file" << endl;
+		cout << "\n1. Insert new key" << endl;
+		cout << "2. Read a Key" << endl;
+		cout << "3. Update Key" << endl;
 		cout << "4. Exit program" << endl;
 
 		cin >> selection;
 
-		//code for menu
-		if(selection == 1)
+		switch (selection)
 		{
+			case 1: 
+				{
+				//getting fileName and value for hash
+		
+				int hashNum;
+		
+				string keyName;
+		
+				cout<< "Enter the key name you'd like to insert: ";
+				cin >> keyName;
+
+				//getting the first hashed value
+				hashNum = hashFunction(keyName);
+
+				//attempting to connect with the clients
+				if(clientInsert(hashNum %7, keyName) < 0)
+				{
+			
+					cout << "first server down unreachable, connecting to next" << endl;
 				
-			//getting fileName and value for hash
-			int hashNum;
-			string fileName;
-			
-			cout<< "Enter file you'd like to read: ";
-			cin >> fileName;
-			readFile(fileName);
-
-			//getting the first hashed value
-			hashNum = hashFunction(fileName);
-
-			//attempting to connect with the clients
-			if(clientWrite(hashNum %7, fileName) < 0)
-			{
-				cout << "first server down unreachable, connecting to next" << endl;
-				if(clientWrite((hashNum +2) % 7, fileName) < 0)
-				{
-					cout << "second server unreachable, connecting to next" << endl;
-					
-					if(clientWrite((hashNum +4) % 7, fileName) < 0)
+					if(clientInsert((hashNum +2) % 7, keyName) < 0)
 					{
-						cout << "None of the servers are reachable, aborting write" << endl;
+
+					
+						cout << "second server unreachable, connecting to next" << endl;
+					
+					
+						if(clientInsert((hashNum +4) % 7, keyName) < 0)
+						{
+							cout << "None of the servers are reachable, aborting write" << endl;
+						}
+				
+			
 					}
 				}
+				
+				break;
+				}
+			case 2:
+			{
+					//getting fileName and value for hash
+				int hashNum;
+				string keyName; 
+			
+				cout<< "Enter key you'd like to read: ";
+				cin >> keyName;
+
+				//getting the first hashed value
+				hashNum = hashFunction(keyName);
+
+				//attempting to connect with the clients
+				if(clientRead(hashNum %7, keyName) < 0)
+				{
+					cout << "first server down unreachable, connecting to next" << endl;
+					if(clientRead((hashNum +2) % 7, keyName) < 0)
+					{
+						cout << "second server unreachable, connecting to next" << endl;
+					
+						if(clientRead((hashNum +4) % 7, keyName) < 0)
+						{
+							cout << "None of the servers are reachable, aborting write" << endl;
+						}
+					}
+				}
+			
+				break;
+			}
+			case 3:
+			{
+				//getting fileName and value for hash
+				int hashNum;
+				string keyName;
+			
+				cout<< "Enter key you'd like to update: ";
+				cin >> keyName;
+
+				//getting the first hashed value
+				hashNum = hashFunction(keyName);
+
+				//attempting to connect with the clients
+				if(clientRead(hashNum %7, keyName) < 0)
+				{
+					cout << "first server down unreachable, connecting to next" << endl;
+					if(clientRead((hashNum +2) % 7, keyName) < 0)
+					{
+						cout << "second server unreachable, connecting to next" << endl;
+					
+						if(clientRead((hashNum +4) % 7, keyName) < 0)
+						{
+							cout << "None of the servers are reachable, aborting write" << endl;
+						}
+					}
+				}
+			
+				break;
+
+			}
+			case 4:
+			{
+
+				cout << "Exitting program" << endl;
+				return 0;
+			}
+			default:
+			{
+				cout << "Invalid Entry, try again" << endl;
+				break;
+
 			}
 			
 
 
 
 		}
-		else if (selection ==2)
-		{
-			//getting fileName and value for hash
-			int hashNum;
-			string fileName;
-			
-			cout<< "Enter file you'd like to read: ";
-			cin >> fileName;
-			readFile(fileName);
-
-			//getting the first hashed value
-			hashNum = hashFunction(fileName);
-
-			//attempting to connect with the clients
-			if(clientRead(hashNum %7, fileName) < 0)
-			{
-				cout << "first server down unreachable, connecting to next" << endl;
-				if(clientRead((hashNum +2) % 7, fileName) < 0)
-				{
-					cout << "second server unreachable, connecting to next" << endl;
-					
-					if(clientRead((hashNum +4) % 7, fileName) < 0)
-					{
-						cout << "None of the servers are reachable, aborting write" << endl;
-					}
-				}
-			}
-			
-
-		}
-		else if (selection ==3)
-		{
-
-		}
-		else if ( selection ==4)
-		{
-			break;
-		}
-		else 
-		{
-			cout << "Error: Invalid selection" << endl;
-		}
+		
 
 	}
 
@@ -275,60 +221,9 @@ int hashFunction(const std::string& obj) {
 
 }
 
-string createFile(){
-	
-	string fileName;
-	string content;
-
-	
-	cout << "Enter file Name: " << endl;
-	cin >> fileName;
-
-	//creating file
-	ofstream MyFile(fileName);
-	
-	cout << "Enter file content: ";
-	cin >> content;
-
-	MyFile << content;
 
 
-	//closing the file
-	MyFile.close();
-
-	return fileName;
-
-}
-
-
-void readFile(string fileName){
-// Create a text string, which is used to output the text file
-
-       	string myText;
-
-
-       	// Read from the text file
-
-       	ifstream MyReadFile(fileName);
-
-
-       	// Use a while loop together with the getline() function to read the file line by line
-
-       	while (getline (MyReadFile, myText)) {
-
-	     	// Output the text from the file
-
-	   	cout << myText;
-     }
-
-     // Close the file
-     MyReadFile.close();
-
-}
-
-
-
-int clientRead(int hashVal, string fileName)
+int clientRead(int hashVal, string keyName)
 {
 
 
@@ -379,7 +274,7 @@ int clientRead(int hashVal, string fileName)
      }
 	
 
-     //handshake protocol
+   /*  //handshake protocol
      //
     fd_set socketfd;
     FD_ZERO(&socketfd);
@@ -406,7 +301,11 @@ int clientRead(int hashVal, string fileName)
 	cout << "No message from server, abort" << endl;
 	return -1;
       }
-
+*/
+      if(!handshake_protocol(sockfd))
+      {
+	cout << "Handshake not completed, aborting" << endl;
+      }
 
       //creating the request
       char request_char[100];
@@ -419,22 +318,29 @@ int clientRead(int hashVal, string fileName)
       string timestampString  = to_string(ms.count());
 
       //getting unique keyvalue from hashing fileName
-     string key = getkeyVal(fileName);
+     string key = getkeyVal(keyName);
 
-    string request = timestampString + ":get:" + key;
+
+     //update or read
+    string request = timestampString + "." +  keyName + "."  + "read";
 
 	converStringToChar(request_char, request); 
 
   	n = write(sockfd, request_char,100);
 
+	n = read(sockfd, buffer, 255);
 
+	printf("Here is the Key value: %s\n", buffer);	
+
+	return 0;
+	
         
 
 }
 
 
-//function to send write request
-int clientWrite(int hashVal, string fileName)
+//function to send update request
+int clientWrite(int hashVal, string keyName)
 {
 
 
@@ -489,7 +395,142 @@ int clientWrite(int hashVal, string fileName)
 
      //handshake protocol
      //
-    fd_set socketfd;
+  /*  fd_set socketfd;
+    FD_ZERO(&socketfd);
+    FD_SET(sockfd, &socketfd);
+
+    //setting timeoutvalue
+    struct timeval timeout;
+    timeout.tv_sec = 5;
+    timeout.tv_usec = 0;
+	
+      n = write(sockfd, "Sending ack", 20);
+
+
+      //waiting for ack message
+      int ret = select(sockfd+1, &socketfd, NULL, NULL, &timeout);
+
+
+      if(ret > 0){
+
+     	 n = read(sockfd,buffer,255);
+      }
+      else
+      {
+	cout << "No message from server, abort" << endl;
+	return -1;
+      }*/
+
+	if(!handshake_protocol(sockfd))
+	{
+		cout << "Handshake not completed, returning back to main\n";
+		return -1;	
+	}
+
+      //creating the request
+      char request_char[100];
+
+      //getting the timestamp
+      milliseconds ms = duration_cast< milliseconds >(
+		    system_clock::now().time_since_epoch()
+		);
+
+      string timestampString  = to_string(ms.count());
+
+
+      //communicating to server
+    string request = timestampString + "." + keyName + ".update";
+
+	converStringToChar(request_char, request); 
+
+  	n = write(sockfd, request_char,100);
+
+	//getting reques to update value
+	n = read(sockfd, buffer, 255);
+
+	printf( buffer);
+
+	//prompting user to enter updated value and creating variables for that process.
+	string keyValue;
+	bzero(buffer, 256);
+	cin >> keyValue;	
+	
+	converStringToChar(buffer, keyValue);
+
+	//sebdubg the updated valuei and getting confirmation
+	write(sockfd, buffer, 255);
+
+	bzero(buffer, 256);
+
+	read(sockfd, buffer, 255);
+
+	printf(buffer);
+
+	return 1;
+	
+
+        
+
+}
+
+
+//function to send update request
+int clientInsert(int hashVal, string keyName)
+{
+
+
+	//arr storying all server names and portnumbers
+      int portArr[7] = {2312, 2313, 2314, 2315, 2316, 2317,2318};
+	string machineName[7] = {"dc01.utdallas.edu", "dc02.utdallas.edu", "dc03.utdallas.edu", "dc04.utdallas.edu", "dc05.utdallas.edu", "dc06.utdallas.edu", "dc07.utdallas.edu"};
+
+
+//	storing the variables needed to create client
+     int sockfd, portno, n;
+     
+     struct sockaddr_in serv_addr;
+     
+     struct hostent *server;
+
+
+
+     char buffer[256];
+
+    //creating client
+     portno = (portArr[hashVal]);
+
+     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+     if (sockfd < 0) {
+
+	    printf("ERROR opening socket");
+	    return -1;
+     }
+
+     server = gethostbyname(machineName[hashVal].c_str());
+
+     if (server == NULL) {
+
+	     fprintf(stderr,"ERROR, no such host\n");
+	     return -1;
+
+     }
+
+     bzero((char *) &serv_addr, sizeof(serv_addr));
+
+     serv_addr.sin_family = AF_INET;
+
+     bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+
+     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+     {
+	     printf("ERROR connecting");
+	     return -1;
+     }
+	
+
+     //handshake protocol
+     //
+   /* fd_set socketfd;
     FD_ZERO(&socketfd);
     FD_SET(sockfd, &socketfd);
 
@@ -514,7 +555,12 @@ int clientWrite(int hashVal, string fileName)
 	cout << "No message from server, abort" << endl;
 	return -1;
       }
-
+*/
+	if(!handshake_protocol(sockfd))
+	{
+		cout << "Handshake not completed, returning back to main\n";
+		return -1;	
+	}
 
       //creating the request
       char request_char[100];
@@ -526,19 +572,36 @@ int clientWrite(int hashVal, string fileName)
 
       string timestampString  = to_string(ms.count());
 
-      //getting unique keyvalue from hashing fileName
-     string key = getkeyVal(fileName);
 
-    string request = timestampString + ":write:" + key;
+      //communicating to server
+    string request = timestampString + "." + keyName + ".insert";
 
 	converStringToChar(request_char, request); 
 
   	n = write(sockfd, request_char,100);
+	printf( buffer);
 
+	//getting value to be sent.
+	string keyValue;
+	bzero(buffer, 256);
+	cin >> keyValue;	
+	
+	converStringToChar(buffer, keyValue);
 
-        
+	//sebdubg the inserted valuei and getting confirmation
+	write(sockfd, buffer, 255);
+
+	bzero(buffer, 256);
+
+	read(sockfd, buffer, 255);
+
+	printf(buffer);
+	
+
+        return 1;
 
 }
+
 
 
 void converStringToChar(char* timeChar, string timeStr){
@@ -565,5 +628,90 @@ string getkeyVal(const std::string& obj) {
 
 
 }
+
+bool handshake_protocol(int server_sock) {
+	  
+      	char server_msg[MAX_HANDSHAKE_MSG_SIZE];
+
+	char client_msg[MAX_HANDSHAKE_MSG_SIZE];
+
+	ssize_t bytes_received, bytes_sent;
+
+	printf("Handshake initiated.\n");
+
+	// Step 1: Client receives the server's handshake message
+
+	     bytes_received = recv(server_sock, server_msg, sizeof(server_msg), 0);
+
+	         if (bytes_received == -1) {
+                 
+			 perror("recv");
+                         return false;
+	   	 }
+
+                        
+		 server_msg[bytes_received] = '\0';
+        
+		 printf("Server: %s\n", server_msg);
+
+		 // Step 2: Client sends its handshake message
+		 const char* client_hello = "Hello! Here is my handshake message.\n";
+		 bytes_sent = send(server_sock, client_hello, strlen(client_hello), 0);
+		 if (bytes_sent == -1) {
+
+			 perror("send");
+
+			 return false;
+
+		 }
+
+		 // Step 3: Client receives the server's response
+		 bytes_received = recv(server_sock, server_msg, sizeof(server_msg), 0);
+		 if (bytes_received == -1) {
+
+			 perror("recv");
+
+			 return false;
+		 }
+
+
+
+		 server_msg[bytes_received] = '\0';
+
+
+
+		 printf("Server: %s\n", server_msg);
+
+
+
+
+
+		 // Step 4: Client sends its confirmation
+
+		 const char* client_response = "Received server response. Handshake complete.\n";
+
+		 bytes_sent = send(server_sock, client_response, strlen(client_response), 0);
+
+
+
+		 if (bytes_sent == -1) {
+
+			 perror("send");
+
+			 return false;
+
+		 }
+
+
+
+		 printf("Handshake complete.\n");
+
+		 // Handshake complete
+
+		 return true;
+
+}
+
+
 
 
